e3d23fe4efd7ccea1349fd85670c9c72e1cf6c32
Joseph Kim <joskim@redhat.com>
2023-06-16 15:23:47 -0400

    Update helm chart install
    Signed-off-by: Joseph Kim <joskim@redhat.com>

--- .ibm/pipelines/kubernetes-tests.sh
+++ .ibm/pipelines/kubernetes-tests.sh
@@ -69,6 +69,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add backstage https://backstage.github.io/charts
 helm repo add janus-idp https://janus-idp.github.io/helm-backstage
 helm repo update
+
 helm upgrade -i backstage backstage/backstage -n backstage -f ./helm/values-k8s-ingress.yaml --wait
   
 echo "Waiting for backstage deployment..."
