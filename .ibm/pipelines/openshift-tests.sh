7bc81514b531085ceac087a65c71816b0c6a4de0
Joseph Kim <joskim@redhat.com>
2023-09-20 16:08:31 -0400

    Update pipeline script
    Signed-off-by: Joseph Kim <joskim@redhat.com>

--- .ibm/pipelines/openshift-tests.sh
+++ .ibm/pipelines/openshift-tests.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
-LOGFILE="pr-${GIT_PR_NUMBER}-openshift-tests-${BUILD_NUMBER}"
-echo "Log file: ${LOGFILE}"
-# source ./.ibm/pipelines/functions.sh
+
+add_helm_repos() {
+    # check installed helm version
+    helm version
+
+    # Check if the bitnami repository already exists
+    if ! helm repo list | grep -q "^bitnami"; then
+        helm repo add bitnami https://charts.bitnami.com/bitnami
+    else
+        echo "Repository bitnami already exists - updating repository instead."
+    fi
+
+    # Check if the backstage repository already exists
+    if ! helm repo list | grep -q "^backstage"; then
+        helm repo add backstage https://backstage.github.io/charts
+    else
+        echo "Repository backstage already exists - updating repository instead."
+    fi
+    
+    # Check if the backstage repository already exists
+    if ! helm repo list | grep -q "^janus-idp"; then
+        helm repo add janus-idp https://janus-idp.github.io/helm-backstage
+    else
+        echo "Repository janus-idp already exists - updating repository instead."
+    fi
+    
+    # Check if the repository already exists
+    if ! helm repo list | grep -q "^${HELM_REPO_NAME}"; then
+        helm repo add "${HELM_REPO_NAME}" "${HELM_REPO_URL}"
+    else
+        echo "Repository ${HELM_REPO_NAME} already exists - updating repository instead."
+    fi
+
+    helm repo update
+}
 
 # install the latest ibmcloud cli on Linux
 install_ibmcloud() {
@@ -12,17 +44,6 @@ install_ibmcloud() {
     echo "the latest ibmcloud cli installed successfully."
   fi
 }
-
-# Call the function to install oc
-install_ibmcloud
-
-ibmcloud version
-ibmcloud config --check-version=false
-ibmcloud plugin install -f container-registry
-ibmcloud plugin install -f kubernetes-service
-
-ibmcloud login -r "${IBM_REGION}" -g "${IBM_RSC_GROUP}" --apikey "${SERVICE_ID_API_KEY}"
-ibmcloud oc cluster config --cluster "${OPENSHIFT_CLUSTER_ID}"
 
 install_oc() {
   if [[ -x "$(command -v oc)" ]]; then
@@ -36,20 +57,11 @@ install_oc() {
   fi
 }
 
-# Call the function to install oc
-install_oc
-
-oc version --client
-oc login -u apikey -p "${SERVICE_ID_API_KEY}" --server="${IBM_OPENSHIFT_ENDPOINT}"
-oc config current-context
-oc project backstage
-
 install_helm() {
   if [[ -x "$(command -v helm)" ]]; then
     echo "Helm is already installed."
   else
     echo "Installing Helm 3 client"
-
     WORKING_DIR=$(pwd)
     mkdir ~/tmpbin && cd ~/tmpbin
 
@@ -62,48 +74,80 @@ install_helm() {
   fi
 }
 
+LOGFILE="pr-${GIT_PR_NUMBER}-openshift-tests-${BUILD_NUMBER}"
+echo "Log file: ${LOGFILE}"
+# source ./.ibm/pipelines/functions.sh
+
+# install ibmcloud
+install_ibmcloud
+
+ibmcloud version
+ibmcloud config --check-version=false
+ibmcloud plugin install -f container-registry
+ibmcloud plugin install -f kubernetes-service
+
+# Using pipeline configuration - environment properties
+ibmcloud login -r "${IBM_REGION}" -g "${IBM_RSC_GROUP}" --apikey "${SERVICE_ID_API_KEY}"
+ibmcloud oc cluster config --cluster "${OPENSHIFT_CLUSTER_ID}"
+
+install_oc
+
+oc version --client
+# oc login -u apikey -p "${SERVICE_ID_API_KEY}" --server="${IBM_OPENSHIFT_ENDPOINT}"
+oc login --token=${K8S_CLUSTER_TOKEN} --server=${K8S_CLUSTER_URL}
+
+# create a name space e.g. rhdh-test
+if ! oc get namespace ${NAME_SPACE} > /dev/null 2>&1; then
+    oc create namespace ${NAME_SPACE}
+else
+    echo "Namespace ${NAME_SPACE} already exists!"
+fi
+
+oc project ${NAME_SPACE}
+
 install_helm
 
-# check installed helm version
-helm version
+add_helm_repos
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add backstage https://backstage.github.io/charts
-helm repo add janus-idp https://janus-idp.github.io/helm-backstage
-helm repo update
 helm upgrade -i backstage janus-idp/backstage -n backstage --wait
+
+# # KNOWN ISSUE: https://issues.redhat.com/browse/OCPBUGSM-37095
+# # Install with CLI then upgrade with CONSOLE is failed
+# helm upgrade -i ${RELEASE_NAME} -n ${NAME_SPACE} ${HELM_REPO_NAME}/${HELM_IMAGE_NAME} -f $DIR/value_files/${HELM_CHART_VALUE_FILE_NAME}
+helm upgrade -i ${RELEASE_NAME} -n ${NAME_SPACE} ${HELM_REPO_NAME}/${HELM_IMAGE_NAME}
 
 echo "Waiting for backstage deployment..."
 sleep 45
 
 oc get pods -n backstage
-oc port-forward -n backstage svc/backstage 7007:http-backend &
-# Store the PID of the background process
-PID=$!
 
-sleep 15
+# oc port-forward -n backstage svc/backstage 7007:http-backend &
+# # Store the PID of the background process
+# PID=$!
 
-# Check if Backstage is up and running
-BACKSTAGE_URL="http://localhost:7007"
-BACKSTAGE_URL_RESPONSE=$(curl -Is "$BACKSTAGE_URL" | head -n 1)
-echo "$BACKSTAGE_URL_RESPONSE"
+# sleep 15
 
-cd $WORKING_DIR/e2e-test
-yarn install
+# # Check if Backstage is up and running
+# BACKSTAGE_URL="http://localhost:7007"
+# BACKSTAGE_URL_RESPONSE=$(curl -Is "$BACKSTAGE_URL" | head -n 1)
+# echo "$BACKSTAGE_URL_RESPONSE"
 
-Xvfb :99 &
-export DISPLAY=:99
+# cd $WORKING_DIR/e2e-test
+# yarn install
 
-# yarn cypress run --headless --browser chrome
-yarn run cypress:run 
+# Xvfb :99 &
+# export DISPLAY=:99
 
-pkill Xvfb
+# # yarn cypress run --headless --browser chrome
+# yarn run cypress:run 
 
-cd $WORKING_DIR
+# pkill Xvfb
 
-# Send Ctrl+C to the process
-kill -INT $PID
+# cd $WORKING_DIR
 
-helm uninstall backstage -n backstage
+# # Send Ctrl+C to the process
+# kill -INT $PID
 
-rm -rf ~/tmpbin
+# helm uninstall backstage -n backstage
+
+# rm -rf ~/tmpbin
